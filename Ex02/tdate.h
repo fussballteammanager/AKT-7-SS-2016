36035c77c70c748dfea4048a2abab6026466da77
manager <ts@beuth.com>
2016-04-16 16:04:06 +0200

    Changed location of projects:
    new folder structure to keep old exercises running
    even if new is being worked on:
    
    git
    -- Aufgaben (pdfs)
    -- Ex01 (Uebung 1)
    -- Ex02 (Uebung 2)

---
+++ Ex02/tdate.h
@@ -0,0 +1,36 @@
+/******************************************************
+ * FUNKTION: Date  class
+ *-----------------------------------------------------
+ * BESCHREIBUNG: class definition
+ * GELTUNGSBEREICH: Global
+ * PARAMETER: Constructor with or without parameters (default values)
+ --------------------------------------------------
+ * ERSTELLT VON: ts
+ *           AM: 10.04.2016
+ * ÄNDERUNGEN  : -
+ ******************************************************/
+
+#ifndef tdate_h
+    #define tdate_h
+
+    class TDate
+    {
+        private:
+            short day;
+            short month;
+            short year;
+
+        public:
+            TDate();
+            TDate(short day, short month, short year);
+            void setDate(short day, short month, short year);
+            /* set date of today */
+            void setCurrentDate();
+            short getDay();
+            short getMonth();
+            short getYear();
+            /* print date */
+            void print();
+    };
+
+#endif
---
+++ Ex02/tscore.h
@@ -0,0 +1,32 @@
+/******************************************************
+ * FUNKTION: Score class
+ *-----------------------------------------------------
+ * BESCHREIBUNG: class definition
+ * GELTUNGSBEREICH: Global
+ * PARAMETER: Constructor with or without parameters (default values)
+ --------------------------------------------------
+ * ERSTELLT VON: ts
+ *           AM: 10.04.2016
+ * ÄNDERUNGEN  : -
+ ******************************************************/
+
+#ifndef tscore_h
+    #define tscore_h
+
+    class TScore
+    {
+        private:
+            short goalsHome;
+            short goalsGuest;
+
+        public:
+            TScore();
+            TScore(short goalsHome, short goalsGuest);
+            void setGoals(short goalsHome, short goalsGuest);
+            short getPointsHome();
+            short getPointsGuest();
+            /* print goals */
+            void print();
+    };
+
+#endif
